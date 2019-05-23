702a5e348f02fa5d2cd9ec2ae0b8113b1685359c
StevenScottOU <50970399+StevenScottOU@users.noreply.github.com>
2019-05-23 18:48:16 -0400

    Add files via upload

---
+++ EventOutcome.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SophProj
+{
+    //outcomes represent the random chance  within each EventOption
+    class EventOutcome
+    {
+        public int chance { get; set; }
+        public string text { get; set; }
+        public string battleValue { get; set; }
+        public IEnumerable<EventReward> rewards { get; set; }
+
+        public EventOutcome()
+        {            
+            
+        }
+    }
+}
