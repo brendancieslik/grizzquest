01d8e6374d4dcded42552349f76d31dd9beec47b
joshuahetak <50969901+joshuahetak@users.noreply.github.com>
2019-05-23 18:49:07 -0400

    Add files via upload

---
+++ CharStats.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System;
+
+public class CharStat
+{
+    public float BaseVal;
+
+    public float Value
+    {
+        get
+        {
+            return CalcFinalVal();
+        }
+    }
+
+    private readonly List<StatMod> statMod;
+
+    public CharStat(float baseVal)
+    {
+        BaseVal = baseVal;
+        statMod = new List<StatMod>();
+    }
+    public void AddMod(StatMod mod)
+    {
+        statMod.Add(mod);
+    }
+    public bool RemoveMod(StatMod mod)
+    {
+        return statMod.Remove(mod);
+    }
+    private float CalcFinalVal()
+    {
+        float finalVal = BaseVal;
+        for(int i = 0; i < statMod.Count; i++)
+        {
+            finalVal += statMod[i].Value; 
+        }
+        return (float)Math.Round(finalVal, 4); 
+    }
+}
