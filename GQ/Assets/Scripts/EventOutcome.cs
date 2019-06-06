4fef109dbc5f13e0c1e5b5b07de099bcb0287fcf
Brendan Cieslik <Brendancieslik@oakland.edu>
2019-06-06 08:53:10 -0400

    Here's the our working copy of the game.

---
+++ GQ/Assets/Scripts/EventOutcome.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventOutcome 
+{
+    public int chance { get; set; }
+    public string text { get; set; }
+    public string battleValue { get; set; }
+    public IEnumerable<EventReward> rewards { get; set; }
+
+    public EventOutcome()
+    {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
